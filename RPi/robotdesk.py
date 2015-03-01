8b73277f5252b82d1ac46c69257f395148a2e418
Brenton Chamberland <bchamberland@gmail.com>
2015-03-01 17:20:23 -0600

    formatting changes, reversed flow to match actual device

--- RPi/robotdesk.py
+++ RPi/robotdesk.py
@@ -3,66 +3,65 @@ import time
 
 
 def extend_actuator(a, b):
-	print("extending...")
-	GPIO.output(a, GPIO.HIGH)
-	GPIO.output(b, GPIO.LOW)	
+    print("extending...")
+    GPIO.output(a, GPIO.LOW)
+    GPIO.output(b, GPIO.HIGH)
 
 
 def retract_actuator(a, b):
-	print("retracting...")
-	GPIO.output(b, GPIO.HIGH)
-	GPIO.output(a, GPIO.LOW)	
+    print("retracting...")
+    GPIO.output(b, GPIO.LOW)
+    GPIO.output(a, GPIO.HIGH)
 
 
 def stop_actuator(a, b):
-	GPIO.output(b, GPIO.LOW)
-	GPIO.output(a, GPIO.LOW)
+    GPIO.output(b, GPIO.LOW)
+    GPIO.output(a, GPIO.LOW)
 
 
 def elevate(distance):
-	extend_time = calculate_time(distance)
-	extend_actuator(relay_1_a_signal, relay_1_b_signal)
-	time.sleep(extend_time)
-	stop_actuator(relay_1_a_signal, relay_1_b_signal)
+    extend_time = calculate_time(distance)
+    extend_actuator(relay_1_a_signal, relay_1_b_signal)
+    time.sleep(extend_time)
+    stop_actuator(relay_1_a_signal, relay_1_b_signal)
 
 
 def lower(distance):
-	lower_time = calculate_time(distance)	
-	retract_actuator(relay_1_a_signal, relay_1_b_signal)
-	time.sleep(lower_time)
-	stop_actuator(relay_1_a_signal, relay_1_b_signal)
+    lower_time = calculate_time(distance)
+    retract_actuator(relay_1_a_signal, relay_1_b_signal)
+    time.sleep(lower_time)
+    stop_actuator(relay_1_a_signal, relay_1_b_signal)
 
 
 def calculate_time(distance):
-	RATE = .6
-	return distance / RATE
+    RATE = .6
+    return distance / RATE
 
 
 def initialize():
-	print("Setting up...")
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(relay_1_a_signal, GPIO.OUT)
-	GPIO.setup(relay_1_b_signal, GPIO.OUT)
-	print("Done setting up")
+    print("Setting up...")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(relay_1_a_signal, GPIO.OUT)
+    GPIO.setup(relay_1_b_signal, GPIO.OUT)
+    print("Done setting up")
 
 
-try:
-	#setup GPIO using BCM numbering
-	relay_1_a_signal = 23
-	relay_1_b_signal = 24
+if __name__ == "__main__":
+    try:
+        #setup GPIO using BCM numbering
+        relay_1_a_signal = 23
+        relay_1_b_signal = 24
 
-	initialize()
-	time.sleep(3)
+        initialize()
+        time.sleep(3)
 
-	elevate(10)
-	time.sleep(5)
-	lower(10)
+        elevate(10)
+        time.sleep(5)
+        lower(10)
 
-finally:
-	GPIO.cleanup()
+    finally:
+        GPIO.cleanup()
 
-
-	
 
 
 
