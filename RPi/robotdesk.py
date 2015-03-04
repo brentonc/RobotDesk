269198f2f7b59684db95ab7df413ebcf65b7b922
Brenton Chamberland <bchamberland@gmail.com>
2015-03-03 21:02:11 -0600

    refactored software to have an Actuator class

--- RPi/robotdesk.py
+++ RPi/robotdesk.py
@@ -2,52 +2,45 @@ import RPi.GPIO as GPIO
 import time
 
 
-def extend_actuator(a, b):
-    print("extending...")
-    GPIO.output(a, GPIO.LOW)
-    GPIO.output(b, GPIO.HIGH)
+class Actuator():
 
+    def __init__(self, relay_a_pin, relay_b_pin):
+        self.relay_a = relay_a_pin
+        self.relay_b = relay_b_pin
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.relay_a, GPIO.OUT)
+        GPIO.setup(self.relay_b, GPIO.OUT)
 
-def retract_actuator(a, b):
-    print("retracting...")
-    GPIO.output(b, GPIO.LOW)
-    GPIO.output(a, GPIO.HIGH)
+    def extend(self):
+        GPIO.output(self.relay_a, GPIO.LOW)
+        GPIO.output(self.relay_b, GPIO.HIGH)
 
+    def retract(self):
+        GPIO.output(self.relay_b, GPIO.LOW)
+        GPIO.output(self.relay_a, GPIO.HIGH)
 
-def stop_actuator(a, b):
-    GPIO.output(b, GPIO.LOW)
-    GPIO.output(a, GPIO.LOW)
+    def stop(self):
+        GPIO.output(self.relay_b, GPIO.LOW)
+        GPIO.output(self.relay_a, GPIO.LOW)
 
+    def elevate(self, distance):
+        extend_time = self.calculate_time(distance)
+        self.extend()
+        time.sleep(extend_time)
+        self.stop()
 
-def reset_actuator():
-    lower(20)
+    def lower(self, distance):
+        lower_time = self.calculate_time(distance)
+        self.retract()
+        time.sleep(lower_time)
+        self.stop()
 
+    def reset(self):
+        self.lower(20)
 
-def elevate(distance):
-    extend_time = calculate_time(distance)
-    extend_actuator(relay_1_a_signal, relay_1_b_signal)
-    time.sleep(extend_time)
-    stop_actuator(relay_1_a_signal, relay_1_b_signal)
-
-
-def lower(distance):
-    lower_time = calculate_time(distance)
-    retract_actuator(relay_1_a_signal, relay_1_b_signal)
-    time.sleep(lower_time)
-    stop_actuator(relay_1_a_signal, relay_1_b_signal)
-
-
-def calculate_time(distance):
-    RATE = .6
-    return distance / RATE
-
-
-def initialize():
-    print("Setting up...")
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(relay_1_a_signal, GPIO.OUT)
-    GPIO.setup(relay_1_b_signal, GPIO.OUT)
-    print("Done setting up")
+    def calculate_time(self, distance):
+        RATE = .6
+        return distance / RATE
 
 
 def cleanup():
@@ -61,12 +54,12 @@ if __name__ == "__main__":
     try:
         #setup GPIO using BCM numbering
 
-        initialize()
+        actuator = Actuator(relay_1_a_signal, relay_1_b_signal)
+        #initialize()
         time.sleep(3)
-
-        elevate(10)
+        actuator.elevate(6)
         time.sleep(5)
-        lower(10)
+        actuator.lower(6)
 
     finally:
         cleanup()
