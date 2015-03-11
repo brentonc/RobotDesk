6cd712887b06c5a01cab3f3c86173d3cdffe9c1f
Brenton Chamberland <bchamberland@gmail.com>
2015-03-11 15:44:30 +0000

    refactored to desk controller

--- RPi/robotdesk.py
+++ RPi/robotdesk.py
@@ -1,39 +1,57 @@
 import RPi.GPIO as GPIO
 import time
 
-
 class Actuator():
 
-    def __init__(self, relay_a_pin, relay_b_pin):
+    def __init__(self, relay_a_pin, relay_b_pin, name):
         self.relay_a = relay_a_pin
         self.relay_b = relay_b_pin
+        self.name = name
+
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.relay_a, GPIO.OUT)
         GPIO.setup(self.relay_b, GPIO.OUT)
 
     def extend(self):
+        print(self.name +' extending')
         GPIO.output(self.relay_a, GPIO.LOW)
         GPIO.output(self.relay_b, GPIO.HIGH)
 
     def retract(self):
+        print(self.name +' retracting')
         GPIO.output(self.relay_b, GPIO.LOW)
         GPIO.output(self.relay_a, GPIO.HIGH)
 
     def stop(self):
+        print(self.name +' stopping')
         GPIO.output(self.relay_b, GPIO.LOW)
         GPIO.output(self.relay_a, GPIO.LOW)
 
+
+class DeskController():
+
+    def __init__(self, actuators):
+        self.actuators = actuators
+        if actuators is None:
+            self.actuators = []
+
     def elevate(self, distance):
         extend_time = self.calculate_time(distance)
-        self.extend()
+        for a in self.actuators:
+            a.extend()
+        
         time.sleep(extend_time)
-        self.stop()
+        for a in self.actuators:
+            a.stop()
 
     def lower(self, distance):
         lower_time = self.calculate_time(distance)
-        self.retract()
+        for a in self.actuators:
+            a.retract()
+        
         time.sleep(lower_time)
-        self.stop()
+        for a in self.actuators:
+            a.stop()
 
     def reset(self):
         self.lower(20)
@@ -47,26 +65,34 @@ def cleanup():
     GPIO.cleanup()
 
 
-relay_1_a_signal = 23
-relay_1_b_signal = 24
+relay_0_a_signal = 23
+relay_0_b_signal = 24
+relay_1_a_signal = 20
+relay_1_b_signal = 21
+relay_2_a_signal = 5
+relay_2_b_signal = 6
 
 if __name__ == "__main__":
     try:
         #setup GPIO using BCM numbering
-
-        actuator = Actuator(relay_1_a_signal, relay_1_b_signal)
+        #actuator = Actuator(relay_1_a_signal, relay_1_b_signal)
         #initialize()
-        time.sleep(3)
-        actuator.elevate(6)
+        #time.sleep(3)
+        #actuator.elevate(6)
+        #time.sleep(5)
+        #actuator.lower(6)
+
+        actuators = [
+            Actuator(relay_0_a_signal, relay_0_b_signal, "Leg#0"),
+            Actuator(relay_1_a_signal, relay_1_b_signal, "Leg#1"),
+            Actuator(relay_2_a_signal, relay_2_b_signal, "Leg#2")]
+        desk = DeskController(actuators)
+        desk.elevate(6)
         time.sleep(5)
-        actuator.lower(6)
+        desk.lower(6)	
 
     finally:
         cleanup()
-
-
-
-
 
 
 
