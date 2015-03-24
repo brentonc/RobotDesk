a938c31e0669f1d8933320f63dd44da168c707e4
Brenton Chamberland <bchamberland@gmail.com>
2015-03-23 20:12:44 -0500

    added support for remembering the current height

--- RPi/robotdesk.py
+++ RPi/robotdesk.py
@@ -3,11 +3,13 @@ import time
 
 class DeskController():
 
-    def __init__(self, relay_a, relay_b, use_gpio):
+    def __init__(self, use_gpio=True, relay_a=4, relay_b=17, ht_file="ht.rd"):
 
         self.use_gpio = use_gpio
         self.relay_a = relay_a
         self.relay_b = relay_b
+        self.height_filename = ht_file
+        self.resetting = False
 
         if self.use_gpio:
             print('Running in GPIO mode')
@@ -49,31 +51,65 @@ class DeskController():
     def elevate(self, distance):
         extend_time = self.calculate_time(distance)
         self.extend()
-
         time.sleep(extend_time)
         self.stop()
+        self.write_height(self.read_height() + distance)
 
     def lower(self, distance):
         lower_time = self.calculate_time(distance)
         self.retract()
-
         time.sleep(lower_time)
         self.stop()
+        if not self.resetting:
+            self.write_height(self.read_height() - distance)
 
     def reset(self):
+        self.resetting = True
         self.lower(20)
+        self.write_height(0)
+        self.resetting = False
 
     def calculate_time(self, distance):
         RATE = .6
         return distance / RATE
 
+    def write_height(self, height):
+        if float(height) < 0:
+            print(('WARNING: height' + height + ' less than zero, reset recommended.'))
+            height = 0
+
+        f = open(self.height_filename, 'w')
+        f.write(str(height))
+        f.close()
+
+    def read_height(self):
+        height = "0"
+        try:
+            f = open(self.height_filename, 'r')
+            height = f.read()
+        except IOError:
+            print('WARNING: height not known.  Reset recommended.')
+            self.write_height(height)  # sets height to 0, may not be correct
+
+        try:
+            float_height = float(height)
+        except:
+            print('WARNING: height file corrupted.  Reset recommended.')
+            float_height = 0
+        return float_height
+
+    def where_am_i(self):
+        print((self.read_height()))
 
 
 if __name__ == "__main__":
-    relay_a_signal = 4
-    relay_b_signal = 17
-
-    desk = DeskController(relay_a_signal, relay_b_signal, False)
-    desk.elevate(6)
-    time.sleep(5)
+    desk = DeskController(False)
+    desk.elevate(2)
+    desk.where_am_i()
+    desk.elevate(1)
+    desk.where_am_i()
+    desk.elevate(3)
+    desk.where_am_i()
+    time.sleep(2)
     desk.lower(6)
+    desk.where_am_i()
